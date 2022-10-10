450a9c2bdbbd0d5ea49c0b9eebe8a5ecfc01ae1c
skgjlske <91312944+skgjlske@users.noreply.github.com>
2022-10-11 05:28:43 +0800

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,16 @@
+ $(document).ready(function () {
+
+// Owl-carousel
+ $('.a-main .owl-carousel').owlCarousel({
+    loop: true,
+    autoplay: true,
+    dots: true,
+    responsive: {
+        0: {
+            items: 1
+        }
+    }
+})
+
+
+});
